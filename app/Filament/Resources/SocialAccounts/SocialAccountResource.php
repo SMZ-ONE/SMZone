ce184a74c568e0b0ecfcba4ee3f3c73c87537421
Ismail Hakki Caglar <smznaturalcosmetic@gmail.com>
2026-07-31 23:43:56 +0200

    feat: sprint 2 skeleton - Content Planner + Products grouped under SMZone

--- app/Filament/Resources/SocialAccounts/SocialAccountResource.php
+++ app/Filament/Resources/SocialAccounts/SocialAccountResource.php
@@ -13,12 +13,19 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class SocialAccountResource extends Resource
 {
     protected static ?string $model = SocialAccount::class;
 
+    protected static string|UnitEnum|null $navigationGroup = 'SMZone';
+
+    protected static ?int $navigationSort = 2;
+
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $navigationLabel = 'Social Accounts';
 
     protected static ?string $recordTitleAttribute = 'account_name';
 
